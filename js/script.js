7dcd043982d92b080ebc496ad7ca4fc365f3d169
Jay0180 <jfbeltran@sscgi.com>
2025-02-28 11:40:06 +0800

    Added Styleshout, Services, and Fixed colors and IMG

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function runTypingEffect() {
-    const text = 'I am Jayson Beltran.';
+    const text = 'Jayson Beltran';
     const typingElement = document.getElementById('typing-text');
     const typingDelay = 100;
 
